41ea71dc58d72bb6123b24b97ad06c8600762ecc
OA Wu <comdan66@gmail.com>
2019-11-03 22:37:06 +0800

    Fix bug

---
+++ sample1.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="zh-Hant">
+  <head>
+    <meta http-equiv="Content-Language" content="zh-tw">
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui">
+
+    <title>商品內頁</title>
+
+    <link rel="stylesheet" type="text/css" href="css/sample1.css">
+
+  </head>
+  <body>
+    <div id='box'>
+      <div class='unit1'>unit1</div>
+      <div class='unit2'>unit2</div>
+      <div class='unit3'>unit3</div>
+    </div>
+  </body>
+</html>
